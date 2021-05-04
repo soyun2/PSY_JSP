fff5b07db40a2cd57440b2512a66e1c3a9bf9d74
soyun2 <tkdtod0619@naver.com>
2021-05-04 10:13:20 +0900

    s

--- spro_mysql/src/spro/com/org/SPRO_DBManager.java
+++ spro_mysql/src/spro/com/org/SPRO_DBManager.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 public class SPRO_DBManager {
 	String ora_class = "oracle.jdbc.driver.OracleDriver";
 	String mysql_class="com.mysql.cj.jdbc.Driver";
-	String ora_url = "jdbc:oracle:thin:@192.168.0.111:1521:xe";
+	String ora_url = "jdbc:oracle:thin:@localhost:1521:xe";
 	String mysql_url = "jdbc:mysql://127.0.0.1:3306/spro?useUnicode=true&characterEncoding=utf8"; 
 	String mysql_id = "root";
 	String mysql_pw = "1234";
